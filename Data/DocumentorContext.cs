eb6fa83f2d138bd8c86a977af8d38139c135454e
Martin Spicuzza <spicuzza157@gmail.com>
2020-04-09 13:12:43 -0500

    finished up DB creation, auto-creates when app is built

--- Data/DocumentorContext.cs
+++ Data/DocumentorContext.cs
@@ -1,12 +1,15 @@
 using Microsoft.EntityFrameworkCore;
 using testMVC.Models;
 
+// Creates Database Context for persisting information
 namespace testMVC.Data
 {
     public class DocumentorContext : DbContext
     {
+        // the App Context
         public DocumentorContext (DbContextOptions<DocumentorContext> options) : base(options){}
 
+        // DB tables w/ models
         public DbSet<Document> Document { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -4,48 +4,47 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-using Microsoft.EntityFrameworkCore;
 using testMVC.Data;
 
-namespace testMVC
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace testMVC {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-                services.AddDbContext<DocumentorContext>(options =>
-            options.UseSqlite(Configuration.GetConnectionString("DocumentorContext")));
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddMvc ();
+
+            // Adds Database context using the connection-string in the appsettings.json file
+            // for production, switch out to PostgreSQL
+            services.AddDbContext<DocumentorContext> (options =>
+                options.UseSqlite (Configuration.GetConnectionString ("DocumentorContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
+            } else {
+                app.UseExceptionHandler ("/Home/Error");
             }
 
-            app.UseStaticFiles();
+            // for auto-creating DB, add to API version
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory> ().CreateScope ()) {
+                var context = serviceScope.ServiceProvider.GetRequiredService<DocumentorContext> ();
+                context.Database.Migrate ();
+            }
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
+            app.UseStaticFiles ();
+
+            app.UseMvc (routes => {
+                routes.MapRoute (
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
